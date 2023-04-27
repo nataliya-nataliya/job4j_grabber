7efd49e5ff643a28f529141913a27967e85ac8d4
nataliya-nataliya <nataliya.chest@gmail.com>
2023-04-27 19:14:03 +0400

    fixed syntax

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -12,8 +12,12 @@ public class Post {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Post post = (Post) o;
         return id == post.id && title.equals(post.title) && link.equals(post.link);
     }
@@ -25,12 +29,13 @@ public class Post {
 
     @Override
     public String toString() {
-        return "Post{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", link='" + link + '\'' +
-                ", description='" + description + '\'' +
-                ", create=" + create +
-                '}';
+        return "Post{"
+                + "id="
+                + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", create=" + create
+                + '}';
     }
 }
