1313a01b4408f7174ff13f00d47da446d91ef11c
nataliya-nataliya <nataliya.chest@gmail.com>
2023-04-27 18:26:46 +0400

    2.1. Date conversion [#289476]

---
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -0,0 +1,11 @@
+package ru.job4j.grabber.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class HabrCareerDateTimeParser implements DateTimeParser{
+    @Override
+    public LocalDateTime parse(String parse) {
+        return LocalDateTime.parse(parse.substring(0, parse.indexOf("+")), DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+    }
+}
---
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.grabber.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HabrCareerDateTimeParserTest {
+
+    @Test
+    void convertedStringWithTimezoneToLocalDateTimeWithoutTimezone() {
+        HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
+        String date = "2023-01-01T12:00:00+03:00";
+        LocalDateTime expected = LocalDateTime.of(2023, 1, 1,12, 0, 0);
+        assertThat(expected).isEqualTo(parser.parse(date));
+    }
+}
