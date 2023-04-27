a72bfe92e8fa1004080d2515cf753d9b4b17444f
nataliya-nataliya <nataliya.chest@gmail.com>
2023-04-27 18:31:17 +0400

    fixed spaces

--- src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -12,7 +12,7 @@ class HabrCareerDateTimeParserTest {
     void convertedStringWithTimezoneToLocalDateTimeWithoutTimezone() {
         HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
         String date = "2023-01-01T12:00:00+03:00";
-        LocalDateTime expected = LocalDateTime.of(2023, 1, 1,12, 0, 0);
+        LocalDateTime expected = LocalDateTime.of(2023, 1, 1, 12, 0, 0);
         assertThat(expected).isEqualTo(parser.parse(date));
     }
 }
