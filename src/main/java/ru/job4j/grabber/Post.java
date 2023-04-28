3182f068b7b34c07225a45bb837b09d09bff94fe
nataliya-nataliya <nataliya.chest@gmail.com>
2023-04-28 14:46:21 +0400

    5. PsqlStore [#285209]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -8,13 +8,61 @@ public class Post {
     private String title;
     private String link;
     private String description;
-    private LocalDateTime create;
+    private LocalDateTime created;
 
-    public Post(String title, String link, String description, LocalDateTime create) {
+    public Post(String title, String link, String description, LocalDateTime created) {
         this.title = title;
         this.link = link;
         this.description = description;
-        this.create = create;
+        this.created = created;
+    }
+
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
     }
 
     @Override
@@ -41,7 +89,7 @@ public class Post {
                 + ", title='" + title + '\''
                 + ", link='" + link + '\''
                 + ", description='" + description + '\''
-                + ", create=" + create
+                + ", create=" + created
                 + '}';
     }
 }
---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,110 @@
+package ru.job4j.grabber;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store {
+    private final Connection cnn;
+
+    public static Properties load() {
+        Properties properties = new Properties();
+        try (InputStream input = PsqlStore.class.getClassLoader().getResourceAsStream("post.properties")) {
+            properties.load(input);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        return properties;
+    }
+
+    public PsqlStore(Properties cfg) {
+        try {
+            Class.forName(cfg.getProperty("driver-class-name"));
+            cnn = DriverManager.getConnection(
+                    cfg.getProperty("url"),
+                    cfg.getProperty("username"),
+                    cfg.getProperty("password")
+            );
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void save(Post post) {
+        String sql = "INSERT INTO post (name, text, link, created) Values (?, ?, ?, ?)" +
+                "on conflict (link) do nothing";
+        try (PreparedStatement preparedStatement = cnn.prepareStatement(sql)) {
+            preparedStatement.setString(1, post.getTitle());
+            preparedStatement.setString(2, post.getDescription());
+            preparedStatement.setString(3, post.getLink());
+            preparedStatement.setObject(4, post.getCreated());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> listPosts = new ArrayList<>();
+        try (PreparedStatement preparedStatement = cnn.prepareStatement("select * from post")) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                listPosts.add(post(resultSet));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return listPosts;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = null;
+        try (PreparedStatement preparedStatement = cnn.prepareStatement("select * from post where id = ?")) {
+
+            preparedStatement.setInt(1, id);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    post = post(resultSet);
+                }
+            }
+            return post;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Post post(ResultSet resultSet) throws SQLException {
+        Timestamp timestamp = resultSet.getTimestamp("created");
+        LocalDateTime localDateTime = timestamp.toLocalDateTime();
+        return new Post(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getString("text"),
+                resultSet.getString("link"),
+                localDateTime
+        );
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cnn != null) {
+            cnn.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        Post post = new Post("java developer", "www.example.com", "java developer job",
+                LocalDateTime.of(2023, 1, 1, 12, 0, 0));
+        PsqlStore psqlStore = new PsqlStore(load());
+        psqlStore.save(post);
+        psqlStore.getAll();
+        psqlStore.findById(2);
+    }
+}
